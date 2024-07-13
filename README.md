6d89576909b95222e19877d3e9b91db5e3c5a40a
Leonardo Mariscal <l@mariscal.ch>
2024-07-13 23:54:47 +0100

    clean-up the project and add readme

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-# micrograd-wgpu
+# micrograd-webgpu
 
 ![Adorable teal fluffy creature sitting in a spring forest](front.jpg)
 
-Yeah, I kinda remembered a bit too late that WGPU is already a Rust lib, not only an acronym.
-But hey, it's a cute name, right?
+WebGPU accelerated [micrograd](https://github.com/karpathy/micrograd).\
+Autograd engine, implementing backpropagation and a simple neural-network library on top.\
+It supports forward and backward passes on both CPU and GPU.
+
+## Usage
+
+It uses [bun](https://bun.sh/) for our js runtime, and bundling.
+
+For managing the build and run commands, it is using just.\
+So install [just](https://github.com/casey/just) if not already installed.
+
+To simply serve the project:
+
+```bash
+just dev
+```
+
+The default port is set to 3000, but you can change it in the justfile.\
+So you can access the project at `http://localhost:3000`.\
+Chrome has been our main target for development, but it should work on other browsers as well.
+
+It should display its progress in the browser's console.\
+And a final forward pass showing the results of the training.
+
+## Examples
+
+The project includes a couple of examples in the test folder.\
+Including training some basic neural networks on some logic gates.\
+You can find those examples in the [tests folder](tests/).
+
+TODO: Add more examples.\
+TODO: Do a comparison between the CPU and GPU implementations.
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,75 @@
 import { Model } from "./nn";
+import { GPUOperations } from "./operations";
+import { Tensor } from "./tensor";
+
+let xor_train = [
+    { input: [0, 0], output: [0] }, // sample
+    { input: [0, 1], output: [1] },
+    { input: [1, 0], output: [1] },
+    { input: [1, 1], output: [0] }
+];
+
+let and_train = [
+    { input: [0, 0], output: [0] },
+    { input: [0, 1], output: [0] },
+    { input: [1, 0], output: [0] },
+    { input: [1, 1], output: [1] }
+];
+
+let continueTraining = false;
 
 async function main() {
-    console.log("Fetching mnist_train.bin");
-    const res = await fetch("/mnist_train.bin");
-    const buffer = await res.arrayBuffer();
-    const u8 = new Uint8Array(buffer);
-    console.log("Fetched mnist_train.bin"); 
+    xor_train;
+    and_train;
+    Tensor.defaultDevice = "cpu";
+    Tensor.debugMode = false;
 
-    const model = new Model([784, 16, 16, 10]);
+    let model = new Model([2, 2, 1]);
+
+    for (let i = 0; i < 1000; i++) {
+        GPUOperations.computePassCount = 0;
+        let loss = new Tensor([0], [1], "loss");
+
+        for (let sample of xor_train) {
+            let x = new Tensor(sample.input, [1, 2]);
+            let desired = new Tensor(sample.output, [1, 1]);
+
+            let y = model.forward(x);
+            let sampleLoss = y.sub(desired).pow(2);
+            loss = loss.add(sampleLoss);
+        }
+        loss = loss.mul(0.25);
+        loss.backward();
+
+        if (loss.device == "wgpu") {
+            await loss.toCPU();
+        }
+
+        if (i % 100 == 0) {
+            console.group("i:", i);
+            console.log("loss:", loss.pretty());
+            let params = model.params;
+            for (let p of params) {
+                // console.log(p.label + "grad", p.grad.pretty());
+            }
+            console.groupEnd();
+        }
+
+        let lr = 0.1;
+        model.learn(lr);
+
+        model.zeroGrad();
+    }
+
+    for (let sample of xor_train) {
+        let x = new Tensor(sample.input, [1, 2]);
+        let y = model.forward(x);
+        if (y.device == "wgpu") {
+            await x.toCPU();
+            await y.toCPU();
+        }
+        console.log("x:", x.pretty(), "y:", y.data[0] > 0.5 ? 1 : 0, "y:", y.pretty());
+    }
 }
 
 main();
